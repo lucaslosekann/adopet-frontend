8561c346c154b971ace2f980bce3c16fd465ef8d
joão fraga <joaofraga60@gmail.com>
2025-03-28 15:35:40 -0300

    Adição de ícone para perfil das ONGs, além da adição de construção inicial da tela do perfil.

--- app/routes/_open/ngo_profile.tsx
+++ app/routes/_open/ngo_profile.tsx
@@ -3,30 +3,27 @@ import { Input } from "../../components/ui/input";
 import { Button } from "../../components/ui/button";
 import { Link } from "react-router";
 
-export default function NgoProfile(props: any) {
-    //use query to get ngo data
+//{JSON.stringify(props.params.id)}
+
+export default function NgoProfile() {
     return (
-        <div className="bg-background-secondary w-screen h-screen flex justify-center items-center">
-            {/* {JSON.stringify(props.params.id)}
-            <div>
-                <img src="/logo.svg" alt="logo" className="w-96 mx-auto" />
-                <h1 className="text-primary-foreground font-secondary tracking-wide text-center text-2xl py-6">
-                    TESTE
-                </h1>
-                <form className="flex flex-col gap-4">
-                    <Input placeholder="Email" name="email" />
-                    <Input placeholder="Password" name="password" type="password" className="text-white" />
-                    <Link to="/register" className="text-primary-foreground text-sm text-center">
-                        Não tem uma conta? Clique aqui para criar
-                    </Link>
-                    <Button
-                        variant="outline"
-                        className="cursor-pointer bg-transparent text-primary-foreground hover:bg-primary-foreground hover:text-primary"
-                    >
-                        Entrar
-                    </Button>
-                </form>
-            </div> */}
+        <div className="bg-white w-screen h-screen flex flex-col">
+            <header className="bg-background-secondary p-4 w-full fixed top-0 left-0">
+                <img src="/logo.svg" alt="logo" className="w-50 mx-auto" />
+            </header>
+            <div className="flex flex-grow justify-center items-center text-center">
+                <div>
+                    <div className="flex items-start text-start">
+                        <img src="/ngo_profile_icon.png" alt="ngo_profile_icon" className="w-40 mx-auto rounded-2xl border-5 border-background-secondary" />
+                        <div className="mt-3 pl-3.5">
+                            <h1 className="font-secondary text-background-secondary text-2xl">Perfil da ONG</h1>
+                            <h1 className="font-secondary text-background-secondary text-5xl font-bold">CACHORRO TOP</h1>
+                            <h1 className="text-background-secondary mt-3 font-semibold">Criado em 01/01/2000.</h1>
+                        </div>
+                    </div>
+                    <h1>popopo</h1>
+                </div>
+            </div>
         </div>
     );
 }
