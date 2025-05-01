20ffa1cc2a8f02c0a41c157976d6138ac2fc95f6
Lucas Losekann <lucas1losekann@gmail.com>
2025-04-30 21:17:29 -0300

    Adoption Profile

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,8 @@ import { index, layout, route, type RouteConfig } from "@react-router/dev/routes
 
 export default [
     layout("./routes/_authenticated/authenticated-layout.tsx", [index("./routes/_authenticated/_index.tsx")]),
-    layout("./routes/_open/open-layout.tsx", [route("login", "./routes/_open/login.tsx")]),
+    layout("./routes/_open/open-layout.tsx", [
+        route("login", "./routes/_open/login.tsx"),
+        route("adocao/:petId", "./routes/_open/adoption_profile.tsx"),
+    ]),
 ] satisfies RouteConfig;
