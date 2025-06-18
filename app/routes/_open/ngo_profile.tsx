8e982276260a1fe7c5c4ff149e8790255e6d10ea
joão fraga <joaofraga60@gmail.com>
2025-06-17 23:17:08 -0300

    Reorganização da tela do perfil da ONG.

--- app/routes/_open/ngo_profile.tsx
+++ app/routes/_open/ngo_profile.tsx
@@ -1,15 +1,15 @@
-import React, { use, useCallback, useEffect, useState } from 'react';
-import { Input } from '../../components/ui/input';
-import { Button } from '../../components/ui/button';
-import { Link } from 'react-router';
-import Header from '~/components/Header';
-import { useQuery } from '@tanstack/react-query';
-import { getOng } from '~/lib/api';
-import Spinner from '~/components/Spinner';
-import { DateTime } from 'luxon';
-import type { AxiosError } from 'axios';
-import { getDataFormatada } from '~/lib/utils';
-import { createStaticPix, hasError } from 'pix-utils';
+import React, { use, useCallback, useEffect, useState } from "react";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
+import { Link } from "react-router";
+import Header from "~/components/Header";
+import { useQuery } from "@tanstack/react-query";
+import { getOng } from "~/lib/api";
+import Spinner from "~/components/Spinner";
+import { DateTime } from "luxon";
+import type { AxiosError } from "axios";
+import { getDataFormatada } from "~/lib/utils";
+import { createStaticPix, hasError } from "pix-utils";
 
 //{JSON.stringify(props.params.id)}
 
@@ -20,7 +20,7 @@ export default function NgoProfile(props: {
 }) {
     const ongId = props.params.id;
     const OngQuery = useQuery({
-        queryKey: ['ong', ongId],
+        queryKey: ["ong", ongId],
         queryFn: () => getOng(ongId),
         refetchOnWindowFocus: false,
     });
@@ -36,7 +36,7 @@ export default function NgoProfile(props: {
                 merchantName: ong.name,
                 merchantCity: ong.address.city,
                 pixKey: ong.pixKey,
-                infoAdicional: 'Doar faz bem!',
+                infoAdicional: "Doar faz bem!",
             });
 
             if (!hasError(pix)) {
@@ -75,49 +75,69 @@ export default function NgoProfile(props: {
     }
 
     return (
-        <div className="bg-white w-screen h-screen flex flex-col">
+        <div className="bg-white min-h-screen flex flex-col">
             <Header />
-            <div className="flex flex-grow justify-center items-center text-center">
-                <div>
-                    <div className="flex items-start text-start justify-center mt-3">
+            <main className="container mx-auto px-4 md:py-16 text-background-secondary">
+                <div className="max-w-4xl mx-auto space-y-12">
+                    <section className="flex flex-col sm:flex-row items-center sm:items-start gap-6 text-center sm:text-left">
                         <img
                             src="/ngo_profile_icon.png"
-                            alt="ngo_profile_icon"
-                            className="w-40 rounded-2xl border-5 border-background-secondary"
+                            alt={`Logo da ONG ${ong.name}`}
+                            className="w-40 h-40 rounded-2xl border-4 border-background-secondary"
                         />
-                        <div className="mt-3 pl-3.5">
-                            <h1 className="font-secondary text-background-secondary text-2xl">Perfil da ONG</h1>
-                            <h1 className="font-secondary text-background-secondary text-5xl font-bold">{ong.name}</h1>
-                            <h1 className="text-background-secondary mt-3 font-semibold">
-                                Criado em {getDataFormatada(ong.createdAt)}.
-                            </h1>
+                        <div className="space-y-1">
+                            <p className="font-secondary text-2xl">Perfil da ONG</p>
+                            <h1 className="font-secondary text-5xl font-bold">{ong.name}</h1>
+                            <p className="text-sm text-gray-500 pt-2">Criado em {getDataFormatada(ong.createdAt)}.</p>
                         </div>
+                    </section>
+                    <section>
+                        <h2 className="text-3xl font-bold font-secondary border-b-2 border-gray-200 pb-2 mb-4">
+                            Sobre a ONG
+                        </h2>
+                        <p className="font-semibold text-lg">{ong.about ?? "Nenhuma descrição informada."}</p>
+                    </section>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12">
+                        <section>
+                            <h2 className="text-3xl font-bold font-secondary border-b-2 border-gray-200 pb-2 mb-4">
+                                Dados
+                            </h2>
+                            <div className="space-y-2 text-lg">
+                                <p className="font-semibold">{ong.phone}</p>
+                                <p className="font-semibold">{ong.email}</p>
+                                <p className="font-semibold">CNPJ {ong.cnpj}</p>
+                                <p className="text-sm text-gray-500 pt-4">
+                                    Última atualização: {getDataFormatada(ong.updatedAt)}.
+                                </p>
+                            </div>
+                        </section>
+                        <section>
+                            <h2 className="text-3xl font-bold font-secondary border-b-2 border-gray-200 pb-2 mb-4">
+                                Nos visite!
+                            </h2>
+                            <div className="space-y-1 text-lg">
+                                <p className="font-semibold">
+                                    {ong.address.city}, {ong.address.uf} - {ong.address.neighborhood}
+                                </p>
+                                <p className="font-normal">
+                                    {ong.address.street}, {ong.address.number}
+                                </p>
+                                <p className="font-normal">CEP: {ong.address.postalCode}</p>
+                            </div>
+                        </section>
                     </div>
-                    <div className="flex flex-col mt-3 items-center">
-                        <h1 className="font-secondary text-background-secondary text-2xl">Sobre a ONG</h1>
-                        <p className="text-background-secondary mt-1 font-semibold">
-                            {ong.about ?? 'Nenhuma descrição informada.'}
-                        </p>
-                        <h1 className="font-secondary text-background-secondary text-2xl mt-4">Dados</h1>
-                        <p className="text-background-secondary mt-1 font-semibold">{ong.phone}</p>
-                        <p className="text-background-secondary mt-1 font-semibold">{ong.email}</p>
-                        <p className="text-background-secondary mt-1 font-semibold">CNPJ {ong.cnpj}</p>
-                        <p className="text-background-secondary mt-1 font-normal">
-                            Última atualização: {getDataFormatada(ong.updatedAt)}.
-                        </p>
-                        <h1 className="font-secondary text-background-secondary text-2xl mt-4">Nos visite!</h1>
-                        <p className="text-background-secondary mt-1 font-semibold">
-                            {ong.address.city}, {ong.address.uf} - {ong.address.neighborhood}
-                        </p>
-                        <p className="text-background-secondary font-normal">
-                            {ong.address.street}, {ong.address.number}
-                        </p>
-                        <p className="text-background-secondary font-normal">CEP: {ong.address.postalCode}</p>
-                        <h1 className="font-secondary text-background-secondary text-2xl mt-4">Doar Faz Bem!</h1>
-                        {qrCodeBase64 && <img className="max-h-40" src={qrCodeBase64} />}
-                    </div>
+                    {qrCodeBase64 && (
+                        <section className="text-center">
+                            <h2 className="text-3xl font-bold font-secondary border-b-2 border-gray-200 pb-2 mb-4 inline-block">
+                                Doar Faz Bem!
+                            </h2>
+                            <div>
+                                <img className="max-h-48 mx-auto" src={qrCodeBase64} />
+                            </div>
+                        </section>
+                    )}
                 </div>
-            </div>
+            </main>
         </div>
     );
 }
